dcfa514c0eb4976a7f6e866da1577a8bd5d331cc
osxmidi <markau0@lycos.com>
2017-03-19 01:35:28 +1100

    Add files via upload

---
+++ remoteplugin.h
@@ -0,0 +1,75 @@
+/*
+  dssi-vst: a DSSI plugin wrapper for VST effects and instruments
+  Copyright 2004-2007 Chris Cannam
+*/
+
+#ifndef REMOTE_PLUGIN_H
+#define REMOTE_PLUGIN_H
+
+#define FIXED_SHM_SIZE  (4 * 65536 * sizeof(float))
+
+const float RemotePluginVersion = 0.986;
+
+enum RemotePluginDebugLevel {
+    RemotePluginDebugNone,
+    RemotePluginDebugSetup,
+    RemotePluginDebugEvents,
+    RemotePluginDebugData
+};
+
+enum RemotePluginOpcode {
+
+    RemotePluginGetVersion = 0,
+    RemotePluginGetName,
+    RemotePluginGetMaker,
+	RemotePluginGetFlags,
+	RemotePluginGetinitialDelay,
+	RemotePluginProcessEvents,
+	RemotePluginGetChunk,
+	RemotePluginSetChunk,
+	// RemotePluginCanBeAutomated,
+	RemotePluginGetProgram,
+	
+	// RemotePluginGetUniqueID,
+	// RemotePluginGetInitialDelay,
+
+
+
+    RemotePluginSetBufferSize = 100,
+    RemotePluginSetSampleRate,
+    RemotePluginReset,
+    RemotePluginTerminate,
+
+    RemotePluginGetInputCount = 200,
+    RemotePluginGetOutputCount,
+
+    RemotePluginGetParameterCount = 300,
+    RemotePluginGetParameterName,
+    RemotePluginSetParameter,
+    RemotePluginGetParameter,
+    RemotePluginGetParameterDefault,
+    RemotePluginGetParameters,
+
+    RemotePluginGetProgramCount = 350,
+    RemotePluginGetProgramName,
+    RemotePluginSetCurrentProgram,
+
+    RemotePluginProcess = 500,
+    RemotePluginIsReady,
+
+    RemotePluginSetDebugLevel = 600,
+    RemotePluginWarn,
+
+    RemotePluginShowGUI = 700,
+    RemotePluginHideGUI,
+
+    RemotePluginGetEffInt = 800,
+    RemotePluginGetEffString,
+    RemotePluginDoVoid,
+	
+    RemotePluginNoOpcode = 9999
+};
+
+class RemotePluginClosedException { };
+
+#endif
