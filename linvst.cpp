14ee3f34bac9f7a0e976a00e236a0144f88e6091
osxmidi <markau0@lycos.com>
2018-01-21 12:00:43 +0000

    Update linvst.cpp

--- linvst.cpp
+++ linvst.cpp
@@ -721,10 +721,7 @@ VST_EXPORT AEffect* VSTPluginMain (audioMasterCallback audioMaster)
         delete plugin;
         return 0;
     }
-	
-    if(plugin->m_runok2 == 1)
-        return 0;
-	
+		
     if(plugin->m_runok == 1)
     {
         if(plugin)
