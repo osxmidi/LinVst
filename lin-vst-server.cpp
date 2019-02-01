86f8c8855b2e6b4120ba3eb913f347a28f5a2fb5
osxmidi <chopinbig@tutanota.com>
2019-02-01 17:25:43 +1100

    Update lin-vst-server.cpp

--- lin-vst-server.cpp
+++ lin-vst-server.cpp
@@ -869,6 +869,11 @@ void RemoteVSTServer::showGUI()
         tryWrite(&m_shm[FIXED_SHM_SIZE], &winm, sizeof(winm));
         return;
     }
+	
+#ifndef EMBED
+    if(melda == 1)
+    m_plugin->dispatcher(m_plugin, effEditClose, 0, 0, 0, 0);
+#endif    
 
    // if (hWnd)
    //     DestroyWindow(hWnd);
