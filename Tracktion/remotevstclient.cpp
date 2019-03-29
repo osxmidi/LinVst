50c3791513bab121aae5a50699d423af8c28181d
osxmidi <chopinbig@tutanota.com>
2019-03-29 21:19:14 +1100

    Update remotevstclient.cpp

--- Tracktion/remotevstclient.cpp
+++ Tracktion/remotevstclient.cpp
@@ -517,7 +517,7 @@ RemoteVSTClient::RemoteVSTClient(audioMasterCallback theMaster) : RemotePluginCl
 
 RemoteVSTClient::~RemoteVSTClient()
 {
-      wait(NULL);
+//     wait(NULL);
 //    while (waitpid(-1, NULL, WNOHANG) > 0) {}
 /*
     for (int i=0;i<5000;i++)
