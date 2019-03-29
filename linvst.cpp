cee36e4fd97f555e35ecea4dcbf2c6e54c1d706a
osxmidi <chopinbig@tutanota.com>
2019-03-29 18:22:52 +1100

    Update linvst.cpp

--- linvst.cpp
+++ linvst.cpp
@@ -655,8 +655,10 @@ if(plugin->runembed == 1)
 
     case effCanDo:
         if (ptr && !strcmp((char *)ptr,"hasCockosExtensions"))
+	{
 	plugin->reaperid = 1;
         plugin->effVoidOp(78345432);
+	}
 #ifdef EMBED
 #ifdef TRACKTIONWM    
         if(plugin->reaperid == 0)
