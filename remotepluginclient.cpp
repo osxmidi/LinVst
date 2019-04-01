7f17f2ba7d926aca5bfcef624c8416980762db1f
osxmidi <chopinbig@tutanota.com>
2019-04-01 20:52:02 +1100

    Update remotepluginclient.cpp

--- remotepluginclient.cpp
+++ remotepluginclient.cpp
@@ -1963,9 +1963,9 @@ void RemotePluginClient::showGUI()
 
 void RemotePluginClient::hideGUI()
 {
-    writeOpcodering(&m_shmControl5->ringBuffer, RemotePluginHideGUI);
-    commitWrite(&m_shmControl5->ringBuffer);
-    waitForServer5();  
+    writeOpcodering(&m_shmControl3->ringBuffer, RemotePluginHideGUI);
+    commitWrite(&m_shmControl3->ringBuffer);
+    waitForServer3();  
 }
 
 #ifdef EMBED
