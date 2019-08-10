07accb3766cbc3c22d993a4e0b6091b24ba85f28
osxmidi <chopinbig@tutanota.com>
2019-08-10 15:31:01 +1000

    Update lin-vst-server.cpp

--- lin-vst-server.cpp
+++ lin-vst-server.cpp
@@ -2175,7 +2175,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdline, int cmds
     cout << "Copyright (c) 2012-2013 Filipe Coelho" << endl;
     cout << "Copyright (c) 2010-2011 Kristian Amlie" << endl;
     cout << "Copyright (c) 2004-2006 Chris Cannam" << endl;
-    cout << "LinVst version 2.54" << endl;
+    cout << "LinVst version 2.62" << endl;
 
     if (cmdline)
     {
