96acfea8cdf352d5c99d9380ac51c095d06dea79
osxmidi <chopinbig@tutanota.com>
2019-08-16 21:26:00 +1000

    Update remoteplugin.h

--- remoteplugin.h
+++ remoteplugin.h
@@ -94,15 +94,9 @@ enum RemotePluginOpcode
     RemotePluginProcessDouble,
     RemoteSetPrecision,
 #endif
-#ifndef MIDIEFF 
-#ifdef VESTIGE
     RemoteInProp,
     RemoteOutProp,
-#endif
-#endif
 #ifdef MIDIEFF
-    RemoteInProp,
-    RemoteOutProp,
     RemoteMidiKey,
     RemoteMidiProgName,
     RemoteMidiCurProg,
