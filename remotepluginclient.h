3159eaeb3a94ab6ec89c5740e076cde8b30bac25
osxmidi <chopinbig@tutanota.com>
2019-09-13 17:38:26 +1000

    Update remotepluginclient.h

--- remotepluginclient.h
+++ remotepluginclient.h
@@ -98,8 +98,8 @@ public:
     bool                getEffOutProp(int index, void *ptr);
         
 #ifdef MIDIEFF
-    bool                getEffInProp(int index, void *ptr);
-    bool                getEffOutProp(int index, void *ptr);
+//    bool                getEffInProp(int index, void *ptr);
+//    bool                getEffOutProp(int index, void *ptr);
     bool                getEffMidiKey(int index, void *ptr);
     bool                getEffMidiProgName(int index, void *ptr);
     bool                getEffMidiCurProg(int index, void *ptr);
