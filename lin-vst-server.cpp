2b203c64a00a6f448c2989681891c88f7b11f76a
osxmidi <chopinbig@tutanota.com>
2020-08-28 22:14:52 +1000

    Update lin-vst-server.cpp

--- lin-vst-server.cpp
+++ lin-vst-server.cpp
@@ -417,10 +417,7 @@ std::string RemoteVSTServer::getMaker()
 void RemoteVSTServer::EffectOpen()
 {
     if (debugLevel > 0)
-        cerr << "dssi-vst-server[1]: opening plugin" << endl;	
-        
-    offset.x = 0;
-    offset.y = 0;        
+        cerr << "dssi-vst-server[1]: opening plugin" << endl;	       
 	
     m_plugin->dispatcher(m_plugin, effOpen, 0, 0, NULL, 0);
 
@@ -487,7 +484,11 @@ void RemoteVSTServer::EffectOpen()
             cerr << "dssi-vst-server: ERROR: Failed to register Windows application class!\n" << endl;
             haveGui = false;
         }
-#ifdef TRACKTIONWM       
+#ifdef TRACKTIONWM   
+		    
+        offset.x = 0;
+        offset.y = 0;    
+		    
     	memset(&wclass2, 0, sizeof(WNDCLASSEX));
         wclass2.cbSize = sizeof(WNDCLASSEX);
         wclass2.style = 0;
