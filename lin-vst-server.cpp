30f603aecd537c7ca9d6fe6c184d356bcb5597e2
osxmidi <chopinbig@tutanota.com>
2020-11-01 17:53:23 +1100

    Update lin-vst-server.cpp

--- lin-vst-server.cpp
+++ lin-vst-server.cpp
@@ -2223,11 +2223,11 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdline, int cmds
     char *libname2 = 0;
     char *fileInfo = 0;
 
-    cout << "DSSI VST plugin server v" << RemotePluginVersion << endl;
-    cout << "Copyright (c) 2012-2013 Filipe Coelho" << endl;
-    cout << "Copyright (c) 2010-2011 Kristian Amlie" << endl;
-    cout << "Copyright (c) 2004-2006 Chris Cannam" << endl;
-    cout << "LinVst version 3.1.5" << endl;
+    cerr << "DSSI VST plugin server v" << RemotePluginVersion << endl;
+    cerr << "Copyright (c) 2012-2013 Filipe Coelho" << endl;
+    cerr << "Copyright (c) 2010-2011 Kristian Amlie" << endl;
+    cerr << "Copyright (c) 2004-2006 Chris Cannam" << endl;
+    cerr << "LinVst version 3.1.5" << endl;
 
     if (cmdline)
     {
@@ -2284,7 +2284,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdline, int cmds
     fileName = fileName.substr(found2 + 1, strlen(libname) - (found2 +1));
     // SetCurrentDirectory(pathName.c_str());
 
-    cout << "Loading  " << libname << endl;
+    cerr << "Loading  " << libname << endl;
 
     HINSTANCE libHandle = 0;
     libHandle = LoadLibrary(libname);
