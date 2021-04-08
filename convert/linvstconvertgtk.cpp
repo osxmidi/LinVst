d53547cc51f31679fd231c718aef785304c3a72e
osxmidi <chopinbig@tutanota.com>
2021-04-08 15:02:32 +1000

    Update linvstconvertgtk.cpp

--- convert/linvstconvertgtk.cpp
+++ convert/linvstconvertgtk.cpp
@@ -192,6 +192,7 @@ int main (int argc, char *argv[])
   gtk_init (&argc, &argv);
 
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_default_size (GTK_WINDOW (window), 300, 300); 
   gtk_window_set_title (GTK_WINDOW (window), "LinVst");
   gtk_container_set_border_width (GTK_CONTAINER (window), 8);
 
