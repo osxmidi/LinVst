f905d1b3a5164d6a532d49ccd3704f57a72f40fb
osxmidi <chopinbig@tutanota.com>
2021-04-17 15:23:00 +1000

    Update remotepluginclient.cpp

--- remotepluginclient.cpp
+++ remotepluginclient.cpp
@@ -687,7 +687,7 @@ void RemotePluginClient::syncStartup() {
   ptr = (int *)m_shm;
 
   for (int i = 0; i < 400000; i++) {
-    if (*ptr == 410) {
+    if (*ptr == 420) {
       startok = 1;
       break;
     }
