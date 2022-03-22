5d459598e4d34b2409f9d64fd7acfa6cde03e839
osxmidi <chopinbig@tutanota.com>
2022-03-22 14:30:38 +1100

    Update lin-vst-server.cpp

--- lin-vst-server.cpp
+++ lin-vst-server.cpp
@@ -1036,7 +1036,7 @@ void RemoteVSTServer::process(float **inputs, float **outputs,
   struct ParamState {
   float value;
   float valueupdate;
-  int changed;
+  char changed;
   };
 */
    
@@ -1070,7 +1070,7 @@ void RemoteVSTServer::processdouble(double **inputs, double **outputs,
   struct ParamState {
   float value;
   float valueupdate;
-  int changed;
+  char changed;
   };
 */
    
@@ -3325,7 +3325,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdline,
   cerr << "Copyright (c) 2012-2013 Filipe Coelho" << endl;
   cerr << "Copyright (c) 2010-2011 Kristian Amlie" << endl;
   cerr << "Copyright (c) 2004-2006 Chris Cannam" << endl;
-  cerr << "LinVst version 4.7.1" << endl;
+  cerr << "LinVst version 4.7.2" << endl;
   
   if (cmdline[0] == '\0') {
     exit(0);
