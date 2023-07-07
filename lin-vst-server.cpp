0fe5c6f3eedddc5137d1209397181378ce4a887a
osxmidi <chopinbig@tutanota.com>
2023-07-07 12:45:24 +1000

    Update lin-vst-server.cpp

--- lin-vst-server.cpp
+++ lin-vst-server.cpp
@@ -1672,6 +1672,7 @@ void RemoteVSTServer::eventloop()
         e.xconfigure.override_redirect = False;
         XSendEvent(display, child, False,
                    StructureNotifyMask | SubstructureRedirectMask, &e);
+	     // if(hWnd)
 	     // MoveWindow(hWnd, xmove, ymove, width, height, true);
         //      }
         break;
