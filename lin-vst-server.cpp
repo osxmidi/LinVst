a37d0a6869ac6b37182767439c2965e6d71aba82
osxmidi <chopinbig@tutanota.com>
2025-03-07 17:38:50 +1100

    Update lin-vst-server.cpp

--- lin-vst-server.cpp
+++ lin-vst-server.cpp
@@ -1996,7 +1996,7 @@ void RemoteVSTServer::showGUI(ShmControl *m_shmControlptr) {
       return;
       }   
       
-      reparentdone = 0;      
+//      reparentdone = 0;      
       
       XSync(display, false);
       
@@ -2109,8 +2109,11 @@ void RemoteVSTServer::hideGUI() {
         x11_win = 0;
 #endif
 
-     if(display && child)
-     XReparentWindow(display, child, XDefaultRootWindow(display), 0, 0);
+      if(display && child)
+      {
+      XUnmapWindow(display, child);
+      XReparentWindow(display, child, XDefaultRootWindow(display), 0, 0);
+      }
 
       XSync(display, false);    
 
